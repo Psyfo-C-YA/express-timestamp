a147dac646c44db80bc4331f74d66d87c557e3e2
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-02-26 16:07:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -32,27 +32,6 @@ app.get("/api/1451001600000", function (req, res) {
 });
 
 
-app.get("/api/timestamp/:date_string", (req, res) => {
-  let dateString = req.params.date_string;
-
-  //A 4 digit number is a valid ISO-8601 for the beginning of that year
-  //5 digits or more must be a unix time, until we reach a year 10,000 problem
-  if (/\d{5,}/.test(dateString)) {
-    let dateInt = parseInt(dateString);
-    //Date regards numbers as unix timestamps, strings are processed differently
-    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
-  } else {
-    let dateObject = new Date(dateString);
-
-    if (dateObject.toString() === "Invalid Date") {
-      res.json({ error: "Invalid Date" });
-    } else {
-      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
-    }
-  }
-});
-
-
 // app.get("/api/:date", function (req, res) {
 //   let dateString = req.params.date;
 
@@ -71,6 +50,46 @@ app.get("/api/timestamp/:date_string", (req, res) => {
 //   }
 
 // });
+
+// app.get("/api/:date", (req, res) => {
+//   let dateString = req.params.date_string;
+
+//   //A 4 digit number is a valid ISO-8601 for the beginning of that year
+//   //5 digits or more must be a unix time, until we reach a year 10,000 problem
+//   if (/\d{5,}/.test(dateString)) {
+//     let dateInt = parseInt(dateString);
+//     //Date regards numbers as unix timestamps, strings are processed differently
+//     res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+//   } else {
+//     let dateObject = new Date(dateString);
+
+//     if (dateObject.toString() === "Invalid Date") {
+//       res.json({ error: "Invalid Date" });
+//     } else {
+//       res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+//     }
+//   }
+// });
+
+
+app.get("/api/:date", (req, res) => {
+  let dateString = req.params.date;
+
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    let dateObject = new Date(dateString);
+
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
+
 
 // Add this route to handle empty date parameter
 app.get("/api", function (req, res) {
