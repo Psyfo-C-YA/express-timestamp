17456509d9967cb0e8a0c6e699613a4f3cf08034
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-02-26 15:39:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greetings: "hello APIs", sajdhaskj: "asjdhaskjd" });
+});
+
+app.get("/api/2015-12-25", function (req, res) {
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+});
+
+app.get("/api/1451001600000", function (req, res) {
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+});
+
+// Add this route to handle invalid date strings
+app.get("/api/:date", function (req, res) {
+  const inputDate = new Date(req.params.date);
+  if (isNaN(inputDate.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    // Continue with the logic for valid dates
+    res.json({ unix: inputDate.getTime(), utc: inputDate.toUTCString() });
+  }
+});
+
+// Add this route to handle empty date parameter
+app.get("/api", function (req, res) {
+  const currentDate = new Date();
+  res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+});
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
